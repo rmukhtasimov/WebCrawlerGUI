c2876127d585a86a96498f33ef858e00da99bc39
BuildTools <unconfigured@null.spigotmc.org>
2023-05-12 06:22:34 -0400

    Add project files.

---
+++ WebCrawlerGUI/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WebCrawlerGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Chartreuse;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(138, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Crawl";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(70, 260);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(243, 169);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 207);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(196, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(451, 200);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(236, 229);
+            this.listBox2.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(86, 141);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(210, 39);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Indexed Sites";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(498, 26);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(150, 113);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.Location = new System.Drawing.Point(464, 145);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(210, 39);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "Word Count";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Button button2;
+        private ListBox listBox2;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+    }
+}
---
+++ WebCrawlerGUI/Form1.cs
@@ -0,0 +1,204 @@
+//////////// This is my Data Structure Final Works Cited - W3Schools, www.learncs.org, ChatGPT, Powerpoints /////////////////////////
+//////////// It crawls the CITS elms website and allows you to use a GUI to search or select on of the pages it discorved through webcrawling. ////////////////////
+
+using System.Diagnostics;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WebCrawlerGUI
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<String, String> HashIndex = new Dictionary<String, String>();
+        Dictionary<String, int> WordCount = new Dictionary<String, int>();
+        String rootSite = "http://citelms.net/Internships/Summer_2018/Fan_Site/";
+        Queue<String> LinksQueue1 = new Queue<String>();
+        List<String> VisitedLinks = new List<String>();
+        WebClient client = new WebClient();
+        int max1 = 0;
+        string max2 = "";
+
+        string webPage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Web Crawl Finished \n Please select or search for more information regarding a specific page");
+            Debug.WriteLine("Hey this is the console");
+            //URLdemo();
+            Crawl();
+        }
+
+        public void Crawl()
+        {
+            //CRAWL///////////////////////////////////////////////////////////////////////////////
+            LinksQueue1.Enqueue(rootSite + "index.html");
+            while (LinksQueue1.Count() > 0)
+            {
+                String temp1 = LinksQueue1.Dequeue();
+                //Debug.WriteLine("The value of temp1 is " + temp1);
+                //Debug.WriteLine(LinksQueue1.Peek());
+                if(!(VisitedLinks.Contains(temp1)))
+                {
+                   
+                    webPage = client.DownloadString(temp1);
+                    VisitedLinks.Add(temp1);
+                    Debug.WriteLine(webPage);
+                    
+                    string pattern1 = "<title>(.*?)</title>";
+                    Match result1 = Regex.Match(webPage, pattern1);
+                    string title = "unknown";
+                    if (result1.Success)
+                    {
+                        title = result1.Groups[1].Value;
+                        Debug.WriteLine("");
+                        Debug.WriteLine("Found title: " + title);
+                        HashIndex.Add(title, temp1);
+                        listBox1.Items.Add(title);
+                    }
+                    string pattern2 = "<a href=\"(.*?)\">";
+                    MatchCollection result2 = Regex.Matches(webPage, pattern2);
+                    foreach (Match m in result2)
+                    {
+                        String link = m.Groups[1].Value;
+                        if (!(link.Contains("http") || link.Contains("void") || link.Contains("pdf")))
+                        {
+                            Debug.WriteLine(link);
+                            LinksQueue1.Enqueue(rootSite + link);
+                        }
+                    }
+                }
+            }
+
+            /*
+            foreach (string i in LinksQueue1)
+            {
+                Debug.WriteLine(i);
+            } */
+            Debug.WriteLine("");
+            foreach (string i in HashIndex.Keys)
+            {
+                Debug.WriteLine(i);
+            }
+            foreach (string i in HashIndex.Values)
+            {
+                Debug.WriteLine(i);
+            }
+        }
+
+        public void WordCounter(String linktemp)                  ///////////////////////////// O(n) It has 3 loops but none of the loops are nested loops so the 3n simplys to O(n)
+        {
+            max1 = 0;
+            max2 = "";
+            WordCount.Clear();
+            listBox2.Items.Clear();
+
+            string link1 = linktemp;
+            webPage = client.DownloadString(link1);
+            
+            string pattern1 = ">(.*?)<";
+            MatchCollection result1 = Regex.Matches(webPage, pattern1);
+            StringBuilder stringbuild = new StringBuilder();
+            foreach (Match m in result1)
+            { 
+                //listBox2.Items.Add(m.Groups[1].Value);
+                stringbuild.Append(m.Groups[1].Value).Append(" ");
+            }
+
+            string page1 = stringbuild.ToString();
+            char[] splitter = { ' ', '.',',' };
+            string[] words = page1.Split(splitter, StringSplitOptions.RemoveEmptyEntries);
+            //MessageBox.Show(page1);
+           
+
+            foreach (string i in words)
+            {
+                
+                if (WordCount.ContainsKey(i))
+                {
+                    WordCount[i]++;
+                }
+                else
+                {
+                    WordCount.Add(i, 1);
+                }
+            }
+
+            foreach (string i in WordCount.Keys)
+            {
+                //Debug.WriteLine(i);  //
+                listBox2.Items.Add(i + "      Amount :" + WordCount[i]);
+                if(i != "and" && i != "the" && i != "at" && i != "of" && i != "a" && i != "to" && i != "in" && i != "is" && i != "their")
+                {
+                    if(WordCount[i] > max1)
+                    {
+                        max1 = WordCount[i];
+                        max2 = i;
+                    }
+                }
+            }
+
+            textBox3.Text= ("The most common word is " + max2 + " appearing " + max1 + " times.");
+
+        }
+
+        public void URLdemo()
+        {
+            
+            WebClient client = new WebClient();
+            string url1 = "https://www.wikipedia.org";
+            string webPage = client.DownloadString(url1);
+            Debug.WriteLine(webPage);
+            string pattern = "<title>(.*?)</title>";
+            // needs using System.Text.RegularExpressions;
+            Match result = Regex.Match(webPage, pattern);
+            string title = "";
+            if (result.Success)
+            {
+                title = result.Groups[1].Value;
+            }
+            MessageBox.Show("Found title: " + title);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox listbox1 = (ListBox)sender;
+            string option1 = listBox1.SelectedItem.ToString();
+            int index1 = listBox1.SelectedIndex;
+            if (option1 != null)
+                WordCounter(HashIndex[option1]);
+                MessageBox.Show("Title: " + option1 + "\n\nLink: " + HashIndex[option1] + "\n\n" + "The Most Common Word on this page is \"" + max2 + "\"");
+                
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string option1 = textBox1.Text;
+            if(option1 != null)
+            {
+                if(HashIndex.ContainsKey(option1))
+                {
+                    WordCounter(HashIndex[option1]);
+                    MessageBox.Show("Found \n" + "Title: " + option1 + "\n\nLink: " + HashIndex[option1] + "\n\n" + "The Most Common Word on this page is \"" + max2 + "\"");
+                }
+                else
+                {
+                    MessageBox.Show("Error ---- Not Found \nPlease Check Spelling");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //WordCounter();  //
+        }
+    }
+    
+}
